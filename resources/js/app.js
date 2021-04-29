fcc9eef9a049fba2e8d32c0baabbc5b577303dd8
Sam Driver <sdriver@gmx.co.uk>
2021-04-29 13:12:38 +0100

    install tailwind scaffolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,9 @@
+import './bootstrap';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueSelect from 'vue-select';
-import Axios from 'axios';
 import Routes from './routes';
 import App from './views/App';
-
-// make Axios available globally (would be nice to inject this dependency, somehow?)
-window.axios = Axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // use VueRouter
 Vue.use(VueRouter)
